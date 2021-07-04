0fa0583ea43105706c36a5eb034bf678ee26eb1e
mohwan <chaoui.mar2@gmail.com>
2021-07-04 17:23:24 +0100

    cleaning

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <Tutorial/>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <a class="flex flex-column justify-center pt-8" href="https://nuxtjs.org" target="_blank">
+        <nuxt-logo-full/>
+      </a>
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <p class="text-center">yo</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
